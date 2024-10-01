e4b8bb83db9641ff9432ffd9c5b75097109be9b1
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-10-01 14:24:37 +0000

    hello name(extra)

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction(firstname):
+
   # My Function
+  print("Hello",firstname,"!")
+
+myFunction("Jomi")
